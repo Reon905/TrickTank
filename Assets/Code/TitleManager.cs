b9c5d56ccce25cfb57a630c3a453ed7156b9a048
Reon905 <h1202515036@hiratagakuen.onmicrosoft.com>
2026-06-22 12:31:34 +0900

    その2

---
+++ Assets/Code/TitleManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField] private string raceSceneName = "Test scene";//シーン名を入れる
+
+    private void Update()
+    {
+        //Enterキーでシーン切り替え
+        if(Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("Test scene");
+        }
+    }
+}
