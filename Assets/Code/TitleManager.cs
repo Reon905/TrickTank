e2e4fedaf67b931f8b0b42ce0ab124c3e72cf96b
Reon905 <h1202515036@hiratagakuen.onmicrosoft.com>
2026-07-16 16:29:44 +0900

    タイトルでステージ選択できるようにする

--- Assets/Code/TitleManager.cs
+++ Assets/Code/TitleManager.cs
@@ -3,8 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
 {
-    public void StartGame()
+    public void Stage1()
     {
         SceneManager.LoadScene("Stage1 Scene");
+    }
+
+    public void Stage2()
+    {
+        SceneManager.LoadScene("Stage2 Scene");
+    }
+
+    public void Stage3()
+    {
+        SceneManager.LoadScene("Stage3 Scene");
     }
 }
---
+++ Assets/Code/TitleManager2.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class TitleManager2 : MonoBehaviour
+{
+
+    public GameObject TitleCanvas;
+    public GameObject StageSelectCanvas;
+
+    //
+    void Start()
+    {
+        TitleCanvas.SetActive(true);
+        StageSelectCanvas.SetActive(false);
+    }
+
+    //スタートボタン押したとき
+    public void OpenStageSelect()
+    {
+        TitleCanvas.SetActive(false);
+        StageSelectCanvas.SetActive(true);
+    }
+
+    //ステージセレクトから戻るボタンを押したとき
+    public void BackToTitle()
+    {
+        TitleCanvas.SetActive(true);
+        StageSelectCanvas.SetActive(false);
+    }
+
+}
